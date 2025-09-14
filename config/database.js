64e8052319266cd3804f6ca4f860e72e91489ba6
byfonz <hello@fonz.work>
2025-09-14 15:41:39 -0700

    Initial Commit

--- config/database.js
+++ config/database.js
@@ -1,10 +1,15 @@
 module.exports = ({ env }) => ({
     connection: {
-        client: 'postgres',
-        connection: {
-            connectionString: env('DATABASE_URL')
-        },
-        debug: true,
-        pool: { min: 0, max: 7 },
-    }
+    client: 'postgres',
+    connection: {
+      host: env('DATABASE_HOST', 'yamanote.proxy.rlwy.net'),
+      port: env.int('DATABASE_PORT', 45096),
+      database: env('DATABASE_NAME', 'railway'),
+      user: env('DATABASE_USERNAME', 'postgres'),
+      password: env('DATABASE_PASSWORD', 'ztbQvSeljJMzLlfcApYYmsNiIJdgYQzS'), // must be a plain string
+      ssl: env.bool('DATABASE_SSL', false),
+    },
+    debug: false,
+    pool: { min: 0, max: 7 },
+  },
 });
--- config/server.js
+++ config/server.js
@@ -2,7 +2,12 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS',[
+      'ca554d1aff19100a554760f70cada8d8bf932d0e380c0c05a9ee275001bdf21e',
+      '3d8a5ae9909396b32f6b47bc7daa0b4e9060578a2274d4465f9454f7c57aa2c4',
+      'ff33bafb8da12d4f299958bbf34d386459836c1818b4b188f8705fa01787b05d',
+      '4ada69673625c053b42b4e0ed394a13894f388879c0296c9f1f894d245e83b74'
+    ]),
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
